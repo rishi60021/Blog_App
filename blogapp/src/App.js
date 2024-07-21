1f777b20591641de3815dac18d00bd436e58709f
rishi60021 <101425794+rishi60021@users.noreply.github.com>
2024-07-21 18:58:52 +0530

    Update App.js

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 import {Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/register" element={user?<Home/>:<Register/>}></Route>
         <Route path="/login" element={user?<Home/>:<Login/>}></Route>
         <Route path="/write" element={user?<Write/>:<Register/>}></Route>
+        <Route path="/settings" element={user?<Settings/>:<Register/>}></Route>
         <Route path="/post/:postid" element={<Single/>}></Route>
       </Routes>
     </>
